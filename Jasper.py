e5865a237910e3881076f46d575e61612cd85aaf
poguepotamus <matthewpogue606@gmail.com>
2023-08-26 12:53:56 -0500

    Added an authentication decorator

--- Jasper.py
+++ Jasper.py
@@ -3,7 +3,7 @@
 from dotenv import dotenv_values
 
 # PyPi Libraries
-from discord import Intents
+from discord import Intents, utils, CategoryChannel, Embed
 from discord.ext import commands
 
 
@@ -20,7 +20,19 @@ CLASSROOM_TEMPLATE = 'Class-Template'
 bot = commands.Bot(command_prefix='/', intents=Intents().all())
 
 
+def is_authorized():
+	# https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#parameter-metadata
+	async def predicate(ctx):
+		return any([
+			ctx.author.id == ctx.guild.owner_id, # Checking if the user is guild owner
+			ctx.author.guild_permissions.administrator, # Checking if the user has admin perms
+			ctx.author.roles[-1].position > utils.get(ctx.guild.roles, name='Jasper').position, # Checking if user has a role above Jasper
+		])
+	return commands.check(predicate)
+
+
 @bot.command()
+@is_authorized()
 async def jasperPing(ctx):
     await ctx.send(f'{bot.user.name} has connected to Discord!')
 
