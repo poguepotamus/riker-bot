7c81e67e7c58621eed59865005fd7b6b7afd6a6d
poguepotamus <matthewpogue606@gmail.com>
2023-08-26 13:10:07 -0500

    Added ability to create/delete a class This creates a role and a copy of the class-template category with all settings preserved, but adds permission for the new class role to see the category. Still need to setup reaction roles for students to sign-up for each class

--- Jasper.py
+++ Jasper.py
@@ -3,7 +3,7 @@
 from dotenv import dotenv_values
 
 # PyPi Libraries
-from discord import Intents, utils, CategoryChannel, Embed
+from discord import Intents, utils, CategoryChannel, Embed, PermissionOverwrite
 from discord.ext import commands
 
 
@@ -29,6 +29,115 @@ def is_authorized():
 			ctx.author.roles[-1].position > utils.get(ctx.guild.roles, name='Jasper').position, # Checking if user has a role above Jasper
 		])
 	return commands.check(predicate)
+
+
+@bot.command()
+@is_authorized()
+async def newClass(ctx, class_name:str):
+	message = ''
+
+	# Creating a new role for the class
+	try:
+		class_role = await ctx.guild.create_role(name=class_name, reason='New class created with name {name}')
+	except Exception:
+		message += f':triangular_flag_on_post: Couln\'t create `{class_name}` role.\n'
+	else:
+		message += f':white_check_mark: Successfully created `{class_name}` role.\n'
+
+	# Creating the new classroom for the class
+	# @TODO Add error handling for if the classroom already exists
+	# @TODO Add error handling for if the classroom can't be created
+	# @TODO Add error handling for if the template doesn't exist
+	try:
+		await createClassroom(ctx, class_name, class_role)
+	except Exception:
+		message += f':triangular_flag_on_post: Couln\'t create `{class_name}` classroom.\n'
+	else:
+		message += f':white_check_mark: Successfully created `{class_name}` classroom.\n'
+
+	# Sending our status message
+	await ctx.send(embed=createStatusEmbed(message, title='Classroom Creation Status'))
+
+
+@bot.command()
+@is_authorized()
+async def deleteClass(ctx, class_name:str):
+	# Attempting to delete the role for this class
+	role = utils.get(ctx.guild.roles, name=class_name)
+	message = ''
+	if role is None:
+		message += f':triangular_flag_on_post: Couln\'t find `{class_name}` role to delete.\n'
+	else:
+		message += f':white_check_mark: Successfully deleted `{class_name}` role.\n'
+		await role.delete(reason='Classroom no longer needed')
+
+	# Attempting to remove the classroom for this class
+	if await deleteClassroom(ctx, utils.get(ctx.guild.categories, name=class_name)):
+		message += f':white_check_mark: Successfully deleted `{class_name}` classroom.\n'
+	else:
+		message += f':triangular_flag_on_post: Couln\'t find `{class_name}` classroom to delete.\n'
+
+	await ctx.send(embed=createStatusEmbed(message, title='Classroom Deletion Status'))
+
+
+async def createClassroom(ctx, class_name, class_role):
+	# Getting the template that we'll be basing our new classroom on
+	template = utils.get(ctx.guild.categories, name=CLASSROOM_TEMPLATE)
+
+	# Creating the new classroom
+	role_permissions = template.overwrites
+	role_permissions[class_role] = PermissionOverwrite(view_channel=True)
+	classroom = await ctx.guild.create_category(class_name,
+		overwrites=role_permissions,
+		reason='New class created with name {name}',
+		position=template.position - 1, # Putting our new category right above the template
+	)
+
+	# Creating all the other channels from our template dynamically
+	for channel in template.channels:
+		new_channel = await channel.clone(reason=f'New class created with name {class_name}', name=channel.name.replace('_name_', class_name))
+		await new_channel.edit(category=classroom, sync_permissions=True)
+
+	# Returning the new class category
+	return classroom
+
+
+async def deleteClassroom(_, classroom:CategoryChannel):
+	# If our search turned up nothing, we return without question
+	if classroom is None:
+		return False
+
+	# Iterating through the channels in the classroom and deleting them
+	for channel in classroom.channels:
+		await channel.delete(reason='Classroom no longer needed')
+
+	# Delete the classroom itself
+	await classroom.delete(reason='Classroom no longer needed')
+
+	return True
+
+
+def createStatusEmbed(message=None, valid=':white_check_mark:', invalid=':triangular_flag_on_post:', **kwargs):
+	# Message is our description if we don't have one
+	if 'description' not in kwargs.keys() and message is not None:
+		kwargs['description'] = message
+
+	# Determining what color we use for the embed
+	if 'color' not in kwargs.keys():
+		# If we have all valid lines, we use green
+		if kwargs['description'].count(valid) == len(kwargs['description'].strip().split('\n')):
+			kwargs['color'] = 0x229922
+		# If we have all invalid lines, we use red
+		elif kwargs['description'].count(invalid) == len(kwargs['description'].strip().split('\n')):
+			kwargs['color'] = 0x992222
+		# If we have a mix, we use orange
+		else:
+			kwargs['color'] = 0xdd8822
+
+	# Sending a message to the user about what happened
+	return Embed(
+		**kwargs,
+	)
 
 
 @bot.command()
